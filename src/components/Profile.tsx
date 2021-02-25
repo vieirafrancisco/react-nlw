e08eda6dc49004f14b7eb8529b64f18ec4f75d9d
Francisco Neto <fvcneto.master98@gmail.com>
2021-02-25 19:45:20 -0300

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,15 @@
+export function Profile() {
+    return (
+        <div className="container">
+            <img src="image.jpg" alt="myprofile_image" />
+            <div className="horizontal-line"/>
+            <div className="profile-name">
+                Francisco Neto
+            </div>
+            <div className="hud">
+                <a href="/">Google</a>
+                <a href="/">Github</a>
+            </div>
+        </div>
+    );
+}
